a9e9abf95ce3e1060cdeb5aa7f421b5859f5d052
Kasim Ali GUL <kasimaligul@gmail.com>
2017-05-13 21:03:40 +0300

    Updated LAMP script with GitHub files

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -33,6 +33,7 @@ service mysqld start
 
 mysql -u root -e "SHOW DATABASES";
 
+
 #Download Starter Content
 cd /vagrant
 sudo -u vagrant wget -q https://raw.githubusercontent.com/ksmlgl/vagrant/master/files/index.html
